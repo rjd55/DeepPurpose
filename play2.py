823341b18e45a11d01e9433e31d2e8d142b8b189
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-08-25 15:11:43 +0100

    Update play2.py

--- play2.py
+++ play2.py
@@ -1,6 +1,35 @@
 import os
 os.chdir('../')
 
-from DeepPurpose import utils, DTI, dataset
+import DeepPurpose.DTI as models
+from DeepPurpose.utils import *
+from DeepPurpose.dataset import *
 
-dataset
+from time import time
+
+t1 = time()
+X_drug, X_target, y = load_process_DAVIS('./data/', binary=False)
+
+drug_encoding = 'CNN'
+target_encoding = 'Transformer'
+train, val, test = data_process(X_drug, X_target, y, 
+                                drug_encoding, target_encoding, 
+                                split_method='random',frac=[0.7,0.1,0.2])
+
+# use the parameters setting provided in the paper: https://arxiv.org/abs/1801.10193
+config = generate_config(drug_encoding = drug_encoding, 
+                         target_encoding = target_encoding, 
+                         cls_hidden_dims = [1024,1024,512], 
+                         train_epoch = 100, 
+                         test_every_X_epoch = 10, 
+                         LR = 0.001, 
+                         batch_size = 128,
+                         hidden_dim_drug = 128,
+                         mpnn_hidden_size = 128,
+                         mpnn_depth = 3, 
+                         cnn_target_filters = [32,64,96],
+                         cnn_target_kernels = [4,8,12]
+                        )
+model = models.model_initialize(**config)
+t2 = time()
+print("cost about " + str(int(t2-t1)) + " seconds")
