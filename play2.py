41a329ff7433bfe90143195fd7ddd3c67792edc5
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-08-25 14:04:13 +0100

    Update play2.py

--- play2.py
+++ play2.py
@@ -5,7 +5,7 @@ from DeepPurpose.dataset import *
 
 #from DeepPurpose import utils, models, dataset
 
-X_drugs, X_targets, y = dataset.load_process_DAVIS(path = './data', binary = False, convert_to_log = True, threshold = 30)
+X_drugs, X_targets, y = DeepPurpose.dataset.load_process_DAVIS(path = './data', binary = False, convert_to_log = True, threshold = 30)
 print('Drug 1: ' + X_drugs[0])
 print('Target 1: ' + X_targets[0])
 print('Score 1: ' + str(y[0]))
