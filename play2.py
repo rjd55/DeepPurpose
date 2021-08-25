b932e7383dcbfe5b1abdd4b578d77d62e9adea6e
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-08-25 12:44:53 +0100

    Update play2.py

--- play2.py
+++ play2.py
@@ -1,3 +1,6 @@
-from DeepPurpose import oneliner
-from DeepPurpose.dataset import *
-oneliner.repurpose(*load_SARS_CoV2_Protease_3CL(), *load_antiviral_drugs(no_cid = True))
+from DeepPurpose import utils, models, dataset
+
+X_drugs, X_targets, y = dataset.load_process_DAVIS(path = './data', binary = False, convert_to_log = True, threshold = 30)
+print('Drug 1: ' + X_drugs[0])
+print('Target 1: ' + X_targets[0])
+print('Score 1: ' + str(y[0]))
