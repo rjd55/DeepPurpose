d6b73708f412fb79dd3cecde2ededc0b4a638bf8
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-08-26 14:28:41 +0100

    Update make_npass_dataset.py

--- make_npass_dataset.py
+++ make_npass_dataset.py
@@ -7,4 +7,6 @@ path_to_text_file = '/content/drive/MyDrive/Colab Notebooks/data/NPASS_for_DeepP
 
 X_drugs, X_targets, y = dataset.read_file_training_dataset_drug_target_pairs(path_to_text_file)
 
-X_drugs
+print(X_drugs[:3])
+print(X_targets[:3])
+print(y[:3])
