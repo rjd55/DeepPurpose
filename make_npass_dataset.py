8601b4aa26dbf60f2a217701d64c4abd828ca776
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-08-26 14:29:38 +0100

    Update make_npass_dataset.py

--- make_npass_dataset.py
+++ make_npass_dataset.py
@@ -6,7 +6,7 @@ from DeepPurpose import utils, DTI, dataset
 path_to_text_file = '/content/drive/MyDrive/Colab Notebooks/data/NPASS_for_DeepPurpose.txt'
 
 X_drugs, X_targets, y = dataset.read_file_training_dataset_drug_target_pairs(path_to_text_file)
-
+print('There are ' + str(len(X_drugs)) + ' drug-target pairs.')
 print(X_drugs[:3])
 print(X_targets[:3])
 print(y[:3])
