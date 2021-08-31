5f1208243d3511bd342a97ea049c9952c8335c5a
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-08-31 15:19:56 +0100

    Update NPASS_tranformer_protein_2.py

--- NPASS_tranformer_protein_2.py
+++ NPASS_tranformer_protein_2.py
@@ -15,15 +15,16 @@ path_to_text_file = '/content/drive/MyDrive/Colab Notebooks/data/NPASS_for_DeepP
 X_drugs, X_targets, y = dataset.read_file_training_dataset_drug_target_pairs(path_to_text_file)
 
 
-
-
+print(X_drugs[:5])
+print(X_targets[:5])
+print(y[:5])
 
 
 drug_encoding = 'CNN'
 target_encoding = 'Transformer'
 train, val, test = data_process(X_drugs, X_targets, y, 
                                 drug_encoding, target_encoding, 
-                                split_method='random',frac=[0.7,0.1,0.2], random_seed = 3)
+                                split_method='random',frac=[0.85,0.1,0.05], random_seed = 3)
 
 # use the parameters setting provided in the paper: https://arxiv.org/abs/1801.10193
 config = generate_config(drug_encoding = drug_encoding, 
@@ -35,7 +36,7 @@ config = generate_config(drug_encoding = drug_encoding,
                          batch_size = 128,
                          hidden_dim_drug = 128,
                          cnn_drug_filters = [32,64,96],
-                         cnn_drug_kernels = [4,8,12],
+                         cnn_drug_kernels = [4,6,8],
                          transformer_n_layer_target = 2
                         )
 model = models.model_initialize(**config)
