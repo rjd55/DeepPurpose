320bdb33f6ed4881e664b0312672bd5b7dbe82c0
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-09-01 13:35:08 +0100

    Update NPASS_compoundTransformer_proteinCNN_4.py

--- NPASS_compoundTransformer_proteinCNN_4.py
+++ NPASS_compoundTransformer_proteinCNN_4.py
@@ -24,16 +24,17 @@ train, val, test = data_process(X_drugs, X_targets, y,
                                 drug_encoding, target_encoding, 
                                 split_method='random',frac=[0.85,0.1,0.05])
 
-#https://academic.oup.com/bioinformatics/article/37/6/830/5929692?login=true
 config = generate_config(drug_encoding = drug_encoding, 
                          target_encoding = target_encoding, 
                          cls_hidden_dims = [1024,1024,512], 
                          train_epoch = 100, 
-                         LR = 0.00001, 
+                         LR = 0.001, 
                          batch_size = 32,
                          cnn_target_filters = [32,64,96],
                          cnn_target_kernels = [4,8,12],
-                         transformer_num_attention_heads_drug = 12,
+                         result_folder = "/content/drive/MyDrive/Colab Notebooks/DeepPurpose_results/NPASS_compoundTransformer_proteinCNN_4",
+                         transformer_n_layer_drug = 10,
+                         
                         )
 
 
