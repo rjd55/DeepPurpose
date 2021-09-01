d4e5589a247b1d53bde6f1d891281c26944a2eda
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-09-01 16:37:04 +0100

    Create NPASS_compoundTransformer_proteinTransformer_5.py

---
+++ NPASS_compoundTransformer_proteinTransformer_5.py
@@ -0,0 +1,48 @@
+import os
+os.chdir('../')
+import DeepPurpose.DTI as models
+from DeepPurpose.utils import *
+from DeepPurpose.dataset import *
+from DeepPurpose import utils, dataset
+import DeepPurpose.DTI as models
+
+from time import time
+
+t1 = time()
+
+path_to_text_file = '/content/drive/MyDrive/Colab Notebooks/data/NPASS_for_DeepPurpose.txt'
+
+X_drugs, X_targets, y = dataset.read_file_training_dataset_drug_target_pairs(path_to_text_file)
+
+print(X_drugs[:5])
+print(X_targets[:5])
+print(y[:5])
+
+drug_encoding = 'Transformer'
+target_encoding = 'Transformer'
+train, val, test = data_process(X_drugs, X_targets, y, 
+                                drug_encoding, target_encoding, 
+                                split_method='random',frac=[0.85,0.1,0.05],random_seed=2)
+
+config = generate_config(drug_encoding = drug_encoding, 
+                         target_encoding = target_encoding, 
+                         cls_hidden_dims = [1024,1024,512], 
+                         train_epoch = 100, 
+                         LR = 0.001, 
+                         batch_size = 32, 
+                         hidden_dim_drug = 128,
+                         transformer_n_layer_target = 2,
+                         transformer_n_layer_drug = 8 #basic setting,
+                         result_folder = "/content/drive/MyDrive/Colab Notebooks/DeepPurpose_results/NPASS_compoundTransformer_proteinTransformer_5"
+                         
+                         
+                    
+                        )
+
+
+model = models.model_initialize(**config)
+t2 = time()
+print("cost about " + str(int(t2-t1)) + " seconds")
+model.train(train, val, test)
+
+model.save_model('/content/drive/MyDrive/Colab Notebooks/models/NPASS_compoundTransformer_proteinTransformer_5')
