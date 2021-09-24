096a3d933dd102ab8d14529006f16188828585a4
rjd55 <80257074+rjd55@users.noreply.github.com>
2021-09-24 15:54:21 +0100

    Update NPASS_predict.py

--- NPASS_predict.py
+++ NPASS_predict.py
@@ -7,16 +7,34 @@ from DeepPurpose import utils, dataset
 import DeepPurpose.DTI as models
 
 from time import time
-
 t1 = time()
+import pandas as pd
+
+test_df_path = '/content/drive/MyDrive/Colab Notebooks/data/NPASS_test_for_deeppurpose.csv'
+test_df = pd.read_csv(test_df_path, error_bad_lines=False, encoding="Latin-1" )
+print(len(test_df))
+
+smiles = list(test_df['SMILES'])
+Target_sequence = list(test_df['Target_sequence'])
+labels = list(test_df['log_IC50'])
 
 
-#path = utils.download_pretrained_model('/content/drive/MyDrive/Colab Notebooks/models/DeepPurpose_IC50_models/NPASS_tranformer_protein_1')
-#net = models.DBTA.model_pretrained(path_dir = path)
-
-#path = '/content/drive/MyDrive/Colab Notebooks/models/DeepPurpose_IC50_models/NPASS_tranformer_protein_1'
-#model = models.DBTA.load_pretrained('/content/drive/MyDrive/Colab Notebooks/models/DeepPurpose_IC50_models/NPASS_tranformer_protein_1')
 model = models.model_pretrained(path_dir = '/content/drive/MyDrive/Colab Notebooks/models/DeepPurpose_IC50_models/NPASS_tranformer_protein_1')
-
-model.config
 print(model.config)
+
+drug_encoding = 'CNN'
+target_encoding = 'Transformer'
+
+X_pred = utils.data_process(smiles, target_sequence, labels, 
+                                drug_encoding, target_encoding, 
+                                split_method='no_split')
+
+
+y_pred = model.predict(X_pred)
+
+df = pd.DataFrame(y_pred)
+df.to_csv('deeppurpose_IC50_predictions.csv')
+!cp deeppurpose_IC50_predictions.csv "/content/drive/MyDrive/Colab Notebooks/data"
+
+print('The predicted score is ' + str(y_pred))
+print(len(y_pred))
